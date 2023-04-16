cc03f49e013e85aec0245b616cdc81ffbf24cff5
punam123456789 <punamp073@gmail.com>
2023-04-16 20:13:07 +0530

    updated

--- server.js
+++ server.js
@@ -1,63 +1,117 @@
-function register(event){
-    
+function register(event) {
+
     // console.log("seccess");
     // alert("success");
     event.preventDefault();
-    var username=document.getElementById("userName").value;
+    var username = document.getElementById("userName").value;
     // console.log(username,"name here");
-    var Email=document.getElementById("userEmail").value;
+    var Email = document.getElementById("userEmail").value;
     // console.log(Email," email here");
-    var pass=document.getElementById("userPassword").value;
+    var pass = document.getElementById("userPassword").value;
     // console.log(pass," password here");
-    var confirmpass=document.getElementById("userConfirmPassword").value;
+    var confirmpass = document.getElementById("userConfirmPassword").value;
+
     // console.log(confirmpass," password here");
 
+    // var userdata = { username: username, email: Email, password: pass, confirmpassword: confirmpass };
+    //        console.log(userdata);
 
-    if( username && Email && pass && confirmpass){
-    if(pass.length>=8 && confirmpass>=8){
-       
-
-            if(pass==confirmpass){
-                var userdata={username:username,email:Email,password:pass,confirmpassword:confirmpass};
-                console.log(userdata);
+    // var LS = JSON.parse(localStorage.getItem("Users")) || [];
+    // LS.push(userdata);
+    // localStorage.setItem("Users", JSON.stringify(LS));
 
 
-                 localStorage.setItem("Users" , JSON.stringify(userdata));
-                // JSON.stringify(); conver-object into JSON
-                // JSON.parse(); conver-json into object
-                // localStorage.setItem(key,value);
-                // localStorage.getItem(key);
-                // localStorage.removeItem(key);
+    //     if (pass == confirmpass) {
+    //         var flag = false; 
+    //         var LS = JSON.parse(localStorage.getItem("Users")) || [];
+    //         console.log(LS);
+    //         for (var i = 0; i < LS.length; i++) {
+    //             if (LS[i].userEmail == Email) {
+    //                 // console.log("match");
+    //                 flag = true;
+    //             }
 
+    //         }
+    //         if(!flag){
+    //             alert("loggin");
+    //          document.getElementById("userName").value = "";
+    //          document.getElementById("userName").value ="";
+    //          document.getElementById("userName").value= "";
+    //          document.getElementById("userName").value ="";
+    //         }
+    //     else{
+    //        alert("match");
+
+    //      }
+    // }
+
+
+
+
+
+
+
+    if (username && Email && pass && confirmpass) {
+        if (pass.length >= 8 && confirmpass.length >= 8) {
+            if (pass == confirmpass) {
+                var flag = false;
+                var LS = JSON.parse(localStorage.getItem("Users")) || [];
+                console.log(LS);
+                for (var i = 0; i < LS.length; i++) {
+                    if (LS[i].email == Email) {
+                        // console.log("match");
+                        flag = true;
+                    }
+                }
+                if (!flag) {
+                    var userdata = { username: username, email: Email, password: pass, confirmpassword: confirmpass };
+                    console.log(userdata);
+
+                    // var LS = JSON.parse(localStorage.getItem("Users")) || [];
+                    LS.push(userdata);
+                    localStorage.setItem("Users", JSON.stringify(LS));
+
+                    alert("Registration succefully");
+
+                    document.getElementById("userName").value = "";
+                    document.getElementById("userEmail").value = "";
+                    document.getElementById("userPassword").value = "";
+                    document.getElementById("userConfirmPassword").value = "";
+                }
+                else {
+                    alert("Email already Present");
+                }
+            } else {
+                console.log("Password not match");
             }
-
-
-            
-
-        else{
-            console.log("password not match");
-           
         }
-    }
-        else{
+        else {
             console.log("password should be 8 or more digits..");
         }
 
-    }else{
-        console.log("fill the all fields required");
-       
     }
+    else {
+        console.log("fill the all fields required");
 
-
-
-
-    
+    }
 }
 
 
-function getdata(){
-   
-    
-    var obj= JSON.parse(localStorage.getItem("Users"))
-    console.log("obj",obj)
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
