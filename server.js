3e110e5878f65d37bf493062632c454f8b59c820
punam123456789 <punamp073@gmail.com>
2023-06-01 10:45:13 +0530

    updated

--- server.js
+++ server.js
@@ -97,35 +97,38 @@ function register(event) {
 }
 
 
-
 // Step 1  : Get user typed data from html to JS = Completed
 // Step 2 : Get all users data into JS from LS = Completed
 // Step 3 : Iterate all users (LS) with user typed data 
 // Step 4 : If email & password both match at same time then
-//  show user suceess
+
+
 function login(event) {
     event.preventDefault();
     alert("Login function called.")
     var userEmail = document.getElementById("email").value;
     var userPassword = document.getElementById("password").value;
-    // console.log(userEmail,userPassword,"check here")
+    // console.log(userEmail,userPassword,"check here");
 
     var LS = JSON.parse(localStorage.getItem("Users"));
-    // console.log(LS, " - LS here")
+    // console.log(LS, " - LS here");
 
     var flag = false;
     for (var i = 0; i < LS.length; i++) {
         console.log(i, LS[i])
+         p
         // console.log(i, LS[i].userEmail, LS[i].userPassword )
         if (LS[i].userEmail == userEmail && LS[i].userPassword == userPassword) {
-            // alert("Email and Pass matchecd")
+            // alert("Email and Pass matchecd");
             flag = true;
         }
     }
     if (flag == true) {
         alert("Login successfull.")
-    } else {
+    } else 
+    {
         alert("Credential not matched.")
+        
     }
 }
 
